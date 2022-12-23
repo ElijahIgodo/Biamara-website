99db957478429f757a9136d4f7daa24026d7931d
Godwin Elijah <120605679+ElijahIgodo@users.noreply.github.com>
2022-12-23 22:37:44 +0100

    Update Biamara.js

--- Biamara.js
+++ Biamara.js
@@ -1,5 +1,412 @@
 
 
+//THis the logo container section
+
+let logoForm1 = document.getElementById("logo-form1")
+
+ function handleClick1(){
+  if (logoForm1.style.display === 'block'){
+    logoForm1.style.display = 'none';
+  }
+  else {
+    logoForm1.style.display = 'block';
+  } 
+ 
+}
+let logoForm2 = document.getElementById("logo-form2")
+
+ function handleClick2(){
+  if (logoForm2.style.display === 'block'){
+    logoForm2.style.display = 'none';
+  }
+  else {
+    logoForm2.style.display = 'block';
+  } 
+}
+let logoForm3 = document.getElementById("logo-form3")
+
+ function handleClick3(){
+  if (logoForm3.style.display === 'block'){
+    logoForm3.style.display = 'none';
+  }
+  else {
+    logoForm3.style.display = 'block';
+  } 
+}
+let logoForm4 = document.getElementById("logo-form4")
+
+ function handleClick4(){
+  if (logoForm4.style.display === 'block'){
+    logoForm4.style.display = 'none';
+  }
+  else {
+    logoForm4.style.display = 'block';
+  } 
+}
+let logoForm5 = document.getElementById("logo-form5")
+
+ function handleClick5(){
+  if (logoForm5.style.display === 'block'){
+    logoForm5.style.display = 'none';
+  }
+  else {
+    logoForm5.style.display = 'block';
+  } 
+}
+let logoForm6 = document.getElementById("logo-form6")
+
+ function handleClick6(){
+  if (logoForm6.style.display === 'block'){
+    logoForm6.style.display = 'none';
+  }
+  else {
+    logoForm6.style.display = 'block';
+  } 
+}
+let logoForm7 = document.getElementById("logo-form7")
+
+ function handleClick7(){
+  if (logoForm7.style.display === 'block'){
+    logoForm7.style.display = 'none';
+  }
+  else {
+    logoForm7.style.display = 'block';
+  } 
+}
+let logoForm8 = document.getElementById("logo-form8")
+
+ function handleClick8(){
+  if (logoForm8.style.display === 'block'){
+    logoForm8.style.display = 'none';
+  }
+  else {
+    logoForm8.style.display = 'block';
+  } 
+}
+let logoForm9 = document.getElementById("logo-form9")
+
+ function handleClick9(){
+  if (logoForm9.style.display === 'block'){
+    logoForm9.style.display = 'none';
+  }
+  else {
+    logoForm9.style.display = 'block';
+  } 
+}
+let logoForm10 = document.getElementById("logo-form10")
+
+ function handleClick10(){
+  if (logoForm10.style.display === 'block'){
+    logoForm10.style.display = 'none';
+  }
+  else {
+    logoForm10.style.display = 'block';
+  } 
+}
+let logoForm11 = document.getElementById("logo-form11")
+
+ function handleClick11(){
+  if (logoForm11.style.display === 'block'){
+    logoForm11.style.display = 'none';
+  }
+  else {
+    logoForm11.style.display = 'block';
+  } 
+}
+let logoForm12 = document.getElementById("logo-form12")
+
+ function handleClick12(){
+  if (logoForm12.style.display === 'block'){
+    logoForm12.style.display = 'none';
+  }
+  else {
+    logoForm12.style.display = 'block';
+  } 
+}
+let logoForm13 = document.getElementById("logo-form13")
+
+ function handleClick13(){
+  if (logoForm13.style.display === 'block'){
+    logoForm13.style.display = 'none';
+  }
+  else {
+    logoForm13.style.display = 'block';
+  } 
+}
+let logoForm14 = document.getElementById("logo-form14")
+
+ function handleClick14(){
+  if (logoForm14.style.display === 'block'){
+    logoForm14.style.display = 'none';
+  }
+  else {
+    logoForm14.style.display = 'block';
+  } 
+}
+let logoForm15 = document.getElementById("logo-form15")
+
+ function handleClick15(){
+  if (logoForm15.style.display === 'block'){
+    logoForm15.style.display = 'none';
+  }
+  else {
+    logoForm15.style.display = 'block';
+  } 
+}
+let logoForm16 = document.getElementById("logo-form16")
+
+ function handleClick16(){
+  if (logoForm16.style.display === 'block'){
+    logoForm16.style.display = 'none';
+  }
+  else {
+    logoForm16.style.display = 'block';
+  } 
+}
+let logoForm17 = document.getElementById("logo-form17")
+
+ function handleClick17(){
+  if (logoForm17.style.display === 'block'){
+    logoForm17.style.display = 'none';
+  }
+  else {
+    logoForm17.style.display = 'block';
+  } 
+}
+let logoForm18 = document.getElementById("logo-form18")
+
+ function handleClick18(){
+  if (logoForm18.style.display === 'block'){
+    logoForm18.style.display = 'none';
+  }
+  else {
+    logoForm18.style.display = 'block';
+  } 
+}
+let logoForm19 = document.getElementById("logo-form19")
+
+ function handleClick19(){
+  if (logoForm19.style.display === 'block'){
+    logoForm19.style.display = 'none';
+  }
+  else {
+    logoForm19.style.display = 'block';
+  } 
+}
+let logoForm20 = document.getElementById("logo-form20")
+
+ function handleClick20(){
+  if (logoForm20.style.display === 'block'){
+    logoForm20.style.display = 'none';
+  }
+  else {
+    logoForm20.style.display = 'block';
+  } 
+}
+let logoForm21 = document.getElementById("logo-form21")
+
+ function handleClick21(){
+  if (logoForm21.style.display === 'block'){
+    logoForm21.style.display = 'none';
+  }
+  else {
+    logoForm21.style.display = 'block';
+  } 
+}
+let logoForm22 = document.getElementById("logo-form22")
+
+ function handleClick22(){
+  if (logoForm22.style.display === 'block'){
+    logoForm22.style.display = 'none';
+  }
+  else {
+    logoForm22.style.display = 'block';
+  } 
+}
+let logoForm23 = document.getElementById("logo-form23")
+
+ function handleClick23(){
+  if (logoForm23.style.display === 'block'){
+    logoForm23.style.display = 'none';
+  }
+  else {
+    logoForm23.style.display = 'block';
+  } 
+}
+let logoForm24 = document.getElementById("logo-form24")
+
+ function handleClick24(){
+  if (logoForm24.style.display === 'block'){
+    logoForm24.style.display = 'none';
+  }
+  else {
+    logoForm24.style.display = 'block';
+  } 
+}
+let logoForm25 = document.getElementById("logo-form25")
+
+ function handleClick25(){
+  if (logoForm25.style.display === 'block'){
+    logoForm25.style.display = 'none';
+  }
+  else {
+    logoForm25.style.display = 'block';
+  } 
+}
+let logoForm26 = document.getElementById("logo-form26")
+
+ function handleClick26(){
+  if (logoForm26.style.display === 'block'){
+    logoForm26.style.display = 'none';
+  }
+  else {
+    logoForm26.style.display = 'block';
+  } 
+}
+let logoForm27 = document.getElementById("logo-form27")
+
+ function handleClick27(){
+  if (logoForm27.style.display === 'block'){
+    logoForm27.style.display = 'none';
+  }
+  else {
+    logoForm27.style.display = 'block';
+  } 
+}
+let logoForm28 = document.getElementById("logo-form28")
+
+ function handleClick28(){
+  if (logoForm28.style.display === 'block'){
+    logoForm28.style.display = 'none';
+  }
+  else {
+    logoForm28.style.display = 'block';
+  } 
+}
+let logoForm29 = document.getElementById("logo-form29")
+
+ function handleClick29(){
+  if (logoForm29.style.display === 'block'){
+    logoForm29.style.display = 'none';
+  }
+  else {
+    logoForm29.style.display = 'block';
+  } 
+}
+let logoForm30 = document.getElementById("logo-form30")
+
+ function handleClick30(){
+  if (logoForm30.style.display === 'block'){
+    logoForm30.style.display = 'none';
+  }
+  else {
+    logoForm30.style.display = 'block';
+  } 
+}
+let logoForm31 = document.getElementById("logo-form31")
+
+ function handleClick31(){
+  if (logoForm31.style.display === 'block'){
+    logoForm31.style.display = 'none';
+  }
+  else {
+    logoForm31.style.display = 'block';
+  } 
+}
+let logoForm32 = document.getElementById("logo-form32")
+
+ function handleClick32(){
+  if (logoForm32.style.display === 'block'){
+    logoForm32.style.display = 'none';
+  }
+  else {
+    logoForm32.style.display = 'block';
+  } 
+}
+let logoForm33 = document.getElementById("logo-form33")
+
+ function handleClick33(){
+  if (logoForm33.style.display === 'block'){
+    logoForm33.style.display = 'none';
+  }
+  else {
+    logoForm33.style.display = 'block';
+  } 
+}
+let logoForm34 = document.getElementById("logo-form34")
+
+ function handleClick34(){
+  if (logoForm34.style.display === 'block'){
+    logoForm34.style.display = 'none';
+  }
+  else {
+    logoForm34.style.display = 'block';
+  } 
+}
+let logoForm35 = document.getElementById("logo-form35")
+
+ function handleClick35(){
+  if (logoForm35.style.display === 'block'){
+    logoForm35.style.display = 'none';
+  }
+  else {
+    logoForm35.style.display = 'block';
+  } 
+}
+let logoForm36 = document.getElementById("logo-form36")
+
+ function handleClick36(){
+  if (logoForm36.style.display === 'block'){
+    logoForm36.style.display = 'none';
+  }
+  else {
+    logoForm36.style.display = 'block';
+  } 
+}
+let logoForm37 = document.getElementById("logo-form37")
+
+ function handleClick37(){
+  if (logoForm37.style.display === 'block'){
+    logoForm37.style.display = 'none';
+  }
+  else {
+    logoForm37.style.display = 'block';
+  } 
+}
+let logoForm38 = document.getElementById("logo-form38")
+
+ function handleClick38(){
+  if (logoForm38.style.display === 'block'){
+    logoForm38.style.display = 'none';
+  }
+  else {
+    logoForm38.style.display = 'block';
+  } 
+}
+let logoForm39 = document.getElementById("logo-form39")
+
+ function handleClick39(){
+  if (logoForm39.style.display === 'block'){
+    logoForm39.style.display = 'none';
+  }
+  else {
+    logoForm39.style.display = 'block';
+  } 
+}
+let logoForm40 = document.getElementById("logo-form40")
+
+ function handleClick40(){
+  if (logoForm40.style.display === 'block'){
+    logoForm40.style.display = 'none';
+  }
+  else {
+    logoForm40.style.display = 'block';
+  } 
+}
+
+
+
+
   //Let's build a calculator
 
   function trash (){
